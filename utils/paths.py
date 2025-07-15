b6ea6f8b8c02b234a7ab38001a2a5d3859946918
Erfan Kheyrollahi <ekm507@gmail.com>
2025-07-15 08:32:36 +0330

    change dirname from .vcdata to .chroni

--- utils/paths.py
+++ utils/paths.py
@@ -12,22 +12,22 @@ def resolve_path(path):
 def get_db_path():
     """Get the path to the database file."""
     home_dir = os.path.expanduser('~')
-    vcdata_dir = os.path.join(home_dir, '.vcdata')
+    vcdata_dir = os.path.join(home_dir, '.chroni')
     return os.path.join(vcdata_dir, 'chroni.db')
 
 def get_text_files_in_directory(directory):
     """Get all text files in a directory recursively."""
     file_storage = FileStorage()
     text_files = []
-    
+
     for root, dirs, files in os.walk(directory):
-        # Skip .vcdata directory
-        if '.vcdata' in dirs:
-            dirs.remove('.vcdata')
-        
+        # Skip .chroni directory
+        if '.chroni' in dirs:
+            dirs.remove('.chroni')
+
         for file in files:
             file_path = os.path.join(root, file)
             if file_storage.is_text_file(file_path):
                 text_files.append(file_path)
-    
+
     return text_files
